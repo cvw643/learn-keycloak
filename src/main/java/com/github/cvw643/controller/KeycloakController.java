646c4b7419769559556801712590d7b247201e2d
huangyuqiang <york@ibsettlecn.com>
2021-10-12 20:28:20 +0800

    initial commit

--- src/main/java/com/github/cvw643/controller/KeycloakController.java
+++ src/main/java/com/github/cvw643/controller/KeycloakController.java
@@ -1,11 +1,15 @@
 package com.github.cvw643.controller;
 
 import org.keycloak.admin.client.Keycloak;
+import org.keycloak.representations.idm.ClientRepresentation;
 import org.keycloak.representations.idm.RealmRepresentation;
+import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequestMapping("/keycloak")
 @RestController
@@ -19,5 +23,15 @@ public class KeycloakController {
     @GetMapping("/realms/{realm}")
     public RealmRepresentation getRealm(@PathVariable String realm) {
         return keycloak.realm(realm).toRepresentation();
+    }
+
+    @GetMapping("/realms/{realm}/clients")
+    public List<ClientRepresentation> getClients(@PathVariable String realm) {
+        return keycloak.realm(realm).clients().findAll();
+    }
+
+    @GetMapping("/realms/{realm}/users")
+    public List<UserRepresentation> getUsers(@PathVariable String realm) {
+        return keycloak.realm(realm).users().list();
     }
 }
